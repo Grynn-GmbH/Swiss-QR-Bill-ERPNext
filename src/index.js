d21cfa211e3c627112750287dcf4c6888099474f
al-chaudhari <chaudharia041@gmail.com>
2021-05-10 21:11:41 +0530

    added new functionalit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 // Swiss QR Bill Library
 import { BlobStream, PDF } from "swissqrbill";
 
-function main(paymentinfo) {
-  const data = {
-    currency: paymentinfo.currency, // Doctype Currency
-    amount: parseInt(paymentinfo.amount), // Doctype Amount
-    reference: paymentinfo.reference, // No Reference
-    creditor: {
-      name: paymentinfo.acfullname, //
-      address: `${paymentinfo.achousenum} ${paymentinfo.acstreet}`, // Address Line 1 & line 2
-      zip: parseInt(paymentinfo.aczipcode), // Bank Account  Code
-      city: paymentinfo.actown, // Bank Account City
-      account: paymentinfo.iban, // Bank Account Iban
-      country: paymentinfo.accountry, // Bank Country
-    },
-    debtor: {
-      name: paymentinfo.payfullname, // Customer Doctype
-      address: `${paymentinfo.payhousenum} ${paymentinfo.paystreet}`, // Address Line 1 & 2
-      zip: parseInt(paymentinfo.payzipcode), // Sales Invoice PCode
-      city: paymentinfo.paytown, // Sales Invoice City
-      country: paymentinfo.paycountry, // Sales Invoice Country
-    },
-  };
-
+function main(paymentinfo, papersize, language) {
+  const data = paymentinfo;
   // Creating A Stream
   const stream = new BlobStream();
-
   // Getting PDF
   const pdf = new PDF(data, stream, {
-    language: paymentinfo.language,
-    size: paymentinfo.papersize,
+    language: language || "DE",
+    size: papersize || "A4",
   });
-
   // On PDF Generation Attach
-  pdf.on("finish", () => {});
+  pdf.on("finish", () => {
+    const url = stream.toBlobURL("application/pdf");
+    triggerDownload(url);
+  });
+}
+
+function triggerDownload(uri) {
+  var evt = new MouseEvent("click", {
+    view: window,
+    bubbles: false,
+    cancelable: true,
+  });
+  var a = document.createElement("a");
+  a.setAttribute("download", "recipt.pdf");
+  a.setAttribute("href", uri);
+  a.setAttribute("target", "_blank");
+  a.dispatchEvent(evt);
 }
 
 const getAddress = async (doctype, docname) => {
@@ -56,7 +50,7 @@ const getAccount = async (companyName) => {
       },
       "iban"
     );
-    return ac;
+    return ac.message;
   } catch (error) {
     console.error(error);
   }
@@ -65,37 +59,43 @@ const getAccount = async (companyName) => {
 window.frappe.ui.form.on("Sales Invoice", {
   on_submit: (frm, cdt, ndt) => {
     console.log(frm);
-    let customer = frm.doc.customer;
-    let currency = frm.doc.currency;
-    let grand_total = frm.doc.grand_total;
-    let company = frm.doc.company;
-    let company_name = frm.doc.company_name;
-    let company_address = frm.doc.company_address_name;
-    let company_account_currency = frm.doc.party_account_currency;
 
-    // const address = getAddress("Address", frm.doc.customer_address_name);
-    // const comapny_address = getAddress(
-    //   "Address",
-    //   frm.doc.customer_address_name
-    // );
-    // window.frappe.db
-    //   .get_value(
-    //     "Bank Account",
-    //     {
-    //       is_default: 1,
-    //       is_company_account: 1,
-    //       company: "Grynn Advanced",
-    //     },
-    //     "iban"
-    //   )
-    console.log(
-      currency,
-      customer,
-      grand_total,
-      company,
-      company_name,
-      company_address,
-      company_account_currency
+    let customer = frm.doc.customer;
+    let amount = frm.doc.grand_total;
+    const ref = frm.doc.docname.split("-");
+    const padding = "0".repeat(27 - ref.length);
+    const reference = `${padding}${ref.pop()}`;
+
+    let company = frm.doc.company;
+    let companyAdderss = getAddress("Address", frm.doc.company_address);
+    let currency = frm.doc.party_account_currency;
+    let customerAddress = getAddress("Address", frm.doc.customer_address);
+    let iban = getAccount(company);
+
+    Promise.all([companyAdderss, customerAddress, iban]).then(
+      (companyAddress, customerAddress, iban) => {
+        const config = {
+          currency,
+          amount,
+          reference,
+          creditor: {
+            name: company, //
+            address: `${companyAddress.address_line1} ${companyAddress.address_line2}`, // Address Line 1 & line 2
+            zip: parseInt(companyAddress.pincode), // Bank Account  Code
+            city: companyAddress.city, // Bank Account City
+            account: iban, // Bank Account Iban
+            country: "US", // Bank Country
+          },
+          debtor: {
+            name: customer, // Customer Doctype
+            address: `${customerAddress.address_line1} ${customerAddress.address_line2}`, // Address Line 1 & 2
+            zip: customerAddress.pincode, // Sales Invoice PCode
+            city: customerAddress.city, // Sales Invoice City
+            country: "US", // Sales Invoice Country
+          },
+        };
+        main(config);
+      }
     );
   },
 });
