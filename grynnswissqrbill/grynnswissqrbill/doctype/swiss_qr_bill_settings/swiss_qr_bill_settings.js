156a8a2b27dbdc3140896da47bc3eef1b29c5853
al-chaudhari <chaudharia041@gmail.com>
2021-05-12 21:06:00 +0530

    added js to swiss qr bill

---
+++ grynnswissqrbill/grynnswissqrbill/doctype/swiss_qr_bill_settings/swiss_qr_bill_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Grynn and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Swiss QR Bill Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
