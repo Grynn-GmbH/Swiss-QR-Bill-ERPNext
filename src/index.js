2e2a7ae9bc56728c0959bcec6a68d901a8bca604
al-chaudhari <chaudharia041@gmail.com>
2021-05-12 13:48:46 +0530

    created reference

--- src/index.js
+++ src/index.js
@@ -55,7 +55,9 @@ window.frappe.ui.form.on("Sales Invoice", {
     showProgress(10, "getting data...");
     let customer = frm.doc.customer;
     let amount = frm.doc.grand_total;
-    const ref = frm.docname.split("-");
+    const _ref = frm.docname.split("-");
+    let ref = `${_ref[1]}${_ref[2]}`;
+    ref = ref.substr(ref.length - 7);
     const _reference = `00000000000${ref[1]}${ref[2]}00000000`;
     const checksum = SwissQRBill.utils.calculateQRReferenceChecksum(_reference);
     const reference = `${_reference}${checksum}`;
