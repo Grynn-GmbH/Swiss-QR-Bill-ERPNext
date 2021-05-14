9f652fbadd6a69f3cc2df1b0e3b9dba30870d1ce
al-chaudhari <chaudharia041@gmail.com>
2021-05-14 23:42:58 +0530

    normalized index file

--- src/index.js
+++ src/index.js
@@ -1,84 +1,13 @@
-import SwissQRBill from "swissqrbill/lib/browser";
-import { generateQRConfig } from "./qrconfig";
-import {
-  showProgress,
-  uploadFileAsAttachment,
-  showError,
-  getLanguageCode,
-  getDocument,
-  getReferenceCode,
-  getCurrency,
-} from "./utils";
-
-function main(paymentinfo, docname, frm, papersize, language) {
-  const data = paymentinfo;
-  const stream = new SwissQRBill.BlobStream();
-  try {
-    const pdf = new SwissQRBill.PDF(data, stream, {
-      language: language || "DE",
-      size: papersize || "A4",
-    });
-
-    showProgress(60, "generating pdf...");
-    pdf.on("finish", () => {
-      // const url = stream.toBlobURL("application/pdf");
-      // const triggerDownload()
-      showProgress(80, "uploading pdf...");
-      uploadFileAsAttachment(stream.toBlob(), docname, frm);
-    });
-  } catch (error) {
-    showError(error);
-  }
-}
-
-const createQRBill = async (frm) => {
-  showProgress(10, "getting data...");
-  const customer = frm.doc.customer;
-  const amount = frm.doc.grand_total;
-  const reference = getReferenceCode(frm.doc.name);
-  const company = frm.doc.company;
-  const language = getLanguageCode(frm.doc.language);
-  const bank = await getDocument("Swiss QR Bill Settings", company);
-  const bankAccount = bank.bank_account;
-  const currency = getCurrency(frm.doc.currency);
-  if (!currency) return;
-
-  const companyAddress = await getDocument("Address", frm.doc.company_address);
-  const customerAddress = await getDocument(
-    "Address",
-    frm.doc.customer_address
-  );
-  const { iban } = await getDocument("Bank Account", bankAccount);
-
-  showProgress(40, "generating pdf...");
-  if (companyAddress.country !== "Switzerland") {
-    showError("Company Should Be Switzerland");
-    return;
-  }
-  const companyCountry = await getDocument("Country", companyAddress.country);
-  const customerCountry = await getDocument("Country", customerAddress.country);
-
-  const companyAddressCode = companyCountry.code.toUpperCase();
-  const customerAddressCode = customerCountry.code.toUpperCase();
-
-  const config = generateQRConfig(
-    currency,
-    amount,
-    company,
-    companyAddress,
-    companyAddressCode,
-    iban,
-    customer,
-    customerAddress,
-    customerAddressCode
-  );
-
-  main(config, frm.docname, frm, "A4", language);
-};
+import { createQRBill } from "./createqrbill";
+import { getReferenceCode } from "./utils";
 
 window.frappe.ui.form.on("Sales Invoice", {
   on_submit: (frm) => {
     createQRBill(frm);
+  },
+
+  onload: (frm) => {
+    frm.doc.esr_reference_code = "";
   },
 
   before_submit: (frm) => {
